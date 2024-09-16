4f6d287d43622b48a1f6f91f04358385c1dc66d9
Suyash Saluja <suyashsaluja@microsoft.com>
2024-09-16 14:37:58 +0530

    checkin main rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+mod cpu;
+mod memory;
+mod ppu;
+mod timer;
+mod interrupts;
+
+use cpu::CPU;
+use memory::Memory;
+use ppu::PPU;
+use timer::Timer;
+use interrupts::InterruptController;
+
+struct Gameboy {
+    cpu: CPU,
+    memory: Memory,
+    ppu: PPU,
+    timer: Timer,
+    interrupt_controller: InterruptController,
+}
+
+impl Gameboy {
+    fn new() -> Self {
+        Gameboy {
+            cpu: CPU::new(),
+            memory: Memory::new(),
+            ppu: PPU::new(),
+            timer: Timer::new(),
+            interrupt_controller: InterruptController::new(),
+        }
+    }
+
+    fn run(&mut self) {
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut gameboy = Gameboy::new();
+    gameboy.run();
 }
